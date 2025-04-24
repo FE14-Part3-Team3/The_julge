f4ab46ad4a6f85248fcc702fd27fba38ce467b77
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-04-24 15:53:42 +0900

    feat: 공통 컴포넌트 모달 구현 (#28)
    * feat: 공통 컴포넌트 모달 구현
    
    * feat: page.tsx 원래 상태로 원복
    
    * refactor: ModalButton 내 static style 객체 컴포넌트 외부로 수정

---
+++ src/components/Button/ModalButton.tsx
@@ -0,0 +1,46 @@
+import clsx from 'clsx';
+
+type Variant = 'primary' | 'outline';
+type Size = 'sm' | 'md' | 'lg';
+
+interface ModalButtonProps {
+  label: string;
+  onClick: () => void;
+  variant?: Variant;
+  size?: Size;
+  className?: string;
+}
+
+const variantStyles: Record<Variant, string> = {
+  primary: 'bg-[#ea3c12] text-white cursor-pointer',
+  outline:
+    'bg-white font-sans text-[#ea3c12] border border-[#ea3c12] cursor-pointer',
+};
+
+const sizeStyles: Record<Size, string> = {
+  sm: 'w-[80px] h-[38px] text-[14px]',
+  md: 'w-[120px] h-[48px] text-[16px]',
+  lg: 'w-[160px] h-[52px] text-[18px]',
+};
+
+export default function ModalButton({
+  label,
+  onClick,
+  variant = 'primary',
+  size = 'md',
+  className = '',
+}: ModalButtonProps) {
+  return (
+    <button
+      onClick={onClick}
+      className={clsx(
+        'font-bold rounded-[8px] flex items-center justify-center',
+        variantStyles[variant],
+        sizeStyles[size],
+        className
+      )}
+    >
+      {label}
+    </button>
+  );
+}
---
+++ src/components/Modal/RegisterModal.tsx
@@ -0,0 +1,34 @@
+import Modal from './Modal';
+import ModalButton from '../Button/ModalButton';
+import { useState } from 'react';
+
+export default function RegisterModal() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <>
+      {isOpen && (
+        <Modal
+          description="내 프로필을 먼저 등록해 주세요."
+          size="compact"
+          layout="top"
+          buttonAlign="center"
+          icon={
+            <img
+              src="/assets/images/notification.svg"
+              alt="알림 아이콘"
+              className="w-6 h-6 mb-3"
+            />
+          }
+        >
+          <ModalButton
+            label="확인"
+            variant="outline"
+            size="sm"
+            onClick={() => setIsOpen(false)}
+          />
+        </Modal>
+      )}
+    </>
+  );
+}
