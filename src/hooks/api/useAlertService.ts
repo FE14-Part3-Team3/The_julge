89e40d98bd01d6a7177c2af7e297f795b6bd8120
songmijin824 <126854066+songmijin824@users.noreply.github.com>
2025-04-27 00:48:13 +0900

    Update useAlertService.ts

--- src/hooks/api/useAlertService.ts
+++ src/hooks/api/useAlertService.ts
@@ -6,58 +6,6 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 export function useUserAlerts(userId: string, query: GetListQuery) { // 유저의 알림 목록 조회
   return useQuery<GetUserAlertsResponse>({
     queryKey: ['user-alerts', userId, query],
-    // queryFn: async () => {
-    //   return {
-    //     offset: query.offset,
-    //     limit: query.limit,
-    //     count: 1,
-    //     hasNext: false,
-    //     items: [
-    //       {
-    //         item: {
-    //           id: 'alert-123',
-    //           createdAt: new Date().toISOString(),
-    //           result: 'accepted',
-    //           read: false,
-    //           application: {
-    //             item: {
-    //               id: 'application-1',
-    //               status: 'pending',
-    //             },
-    //             href: '',
-    //           },
-    //           shop: {
-    //             item: {
-    //               id: 'shop-1',
-    //               name: '테스트 가게',
-    //               category: '한식',
-    //               address1: '서울시 강남구',
-    //               address2: '1층',
-    //               description: '맛있는 집',
-    //               imageUrl: '',
-    //               originalHourlyPay: 10000,
-    //             },
-    //             href: '',
-    //           },
-    //           notice: {
-    //             item: {
-    //               id: 'notice-1',
-    //               hourlyPay: 10000,
-    //               description: '알바 공고 설명',
-    //               startsAt: new Date().toISOString(),
-    //               workhour: 8,
-    //               closed: false,
-    //             },
-    //             href: '',
-    //           },
-    //           links: [],
-    //         },
-    //         links: [],
-    //       },
-    //     ],
-    //     links: [], 
-    //   } as GetUserAlertsResponse;
-    // },
     queryFn: async () => {
       const token = localStorage.getItem('token');
       const res = await requestor.get<GetUserAlertsResponse>(`/users/${userId}/alerts`, {
