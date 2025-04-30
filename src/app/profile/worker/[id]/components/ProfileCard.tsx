9f82b389ce3006cb5af30defd4dceddc5b20d73a
이나경 <leena3823@gmail.com>
2025-04-30 19:30:52 +0900

    feat: 유저 정보 가져오기 react query 구현

--- src/app/profile/worker/[id]/components/ProfileCard.tsx
+++ src/app/profile/worker/[id]/components/ProfileCard.tsx
@@ -2,22 +2,27 @@
 
 import Button from '@/components/Button/Button';
 import IconTextList from '@/components/IconText/IconTextList';
+// import { useParams } from 'next/navigation';
+import { useGetUser } from '@/hooks/api/useUserService';
 
-interface ProfileCardProps {
-  name: string;
-  phone: string;
-  region: string;
-  introduction: string;
-  onEdit?: () => void;
-}
+// interface ProfileCardProps {
+//   name: string;
+//   phone: string;
+//   region: string;
+//   introduction: string;
+//   onEdit?: () => void;
+// }
 
-export default function ProfileCard({
-  name,
-  phone,
-  region,
-  introduction,
-  onEdit,
-}: ProfileCardProps) {
+export default function ProfileCard() {
+  const userId = '83bd130d-e852-4dac-b6f5-b0c9922d6175';
+  const { data } = useGetUser(userId);
+
+  if (!data) {
+    return null;
+  }
+
+  const { name, phone, address, bio } = data.item;
+
   return (
     <section className="w-full sm:max-w-[665px] min-h-[256px] sm:p-[32px] p-[20px] bg-[#FFEBE7] flex flex-col gap-[12px] justify-between rounded-[12px]">
       <div className="flex justify-between items-center">
@@ -33,7 +38,6 @@ export default function ProfileCard({
           variant="outline"
           size="md"
           className="w-[108px] h-[37px] sm:w-[169px] sm:h-[48px]"
-          onClick={onEdit}
         >
           편집하기
         </Button>
@@ -41,16 +45,16 @@ export default function ProfileCard({
 
       <IconTextList
         items={[
-          { iconSrc: '/assets/images/phone.svg', text: phone },
+          { iconSrc: '/assets/images/phone.svg', text: phone ?? '' },
           {
             iconSrc: '/assets/images/map.svg',
-            text: `선호지역: ${region}`,
+            text: `선호지역: ${address}`,
           },
         ]}
       />
 
       <p className="text-black font-spoqa text-[14px] sm:text-[16px] font-normal leading-[26px]">
-        {introduction}
+        {bio}
       </p>
     </section>
   );
---
+++ src/hooks/api/useUserService.ts
@@ -0,0 +1,16 @@
+import { useQuery } from '@tanstack/react-query';
+import requestor from '@/lib/axios';
+import type { GetUserResponse } from '@/types/api/user';
+
+export const useGetUser = (userId: string) => {
+  // 유저 정보 조회
+  return useQuery<GetUserResponse>({
+    queryKey: ['users', userId],
+    queryFn: async () => {
+      const res = await requestor.get<GetUserResponse>(`/users/${userId}`);
+      console.log(res.data);
+      return res.data;
+    },
+    enabled: !!userId,
+  });
+};
