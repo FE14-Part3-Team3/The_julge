c4c73c61be3b70509e6c4b742d0f4e69f6c64268
이나경 <leena3823@gmail.com>
2025-04-30 20:40:09 +0900

    feat: 프로필 등록 여부에 따라 조건부 렌더링 구현

--- src/app/profile/worker/[id]/page.tsx
+++ src/app/profile/worker/[id]/page.tsx
@@ -18,24 +18,38 @@ export default function WorkerProfilePage() {
 
   return (
     <>
-      <div className="flex flex-col sm:flex-row w-full max-w-[964px] mx-auto px-6 py-[60px] gap-[24px] sm:gap-[180px]">
-        <p className="sm:text-[28px] text-black font-spoqa text-[20px] font-bold leading-normal tracking-[0.56px] whitespace-nowrap">
-          내 프로필
-        </p>
-        <ProfileCard
-          name={name ?? ''}
-          phone={phone ?? ''}
-          address={address ?? ''}
-          bio={bio ?? ''}
-        />
-      </div>
-      <div className="bg-gray-5">
+      {name ? ( // name이 있을 때, 즉 프로필 등록이 완료 되었을 때 보이는 UI
+        <>
+          <div className="flex flex-col sm:flex-row w-full max-w-[964px] mx-auto px-6 py-[60px] gap-[24px] sm:gap-[180px]">
+            <p className="sm:text-[28px] text-black font-spoqa text-[20px] font-bold leading-normal tracking-[0.56px] whitespace-nowrap">
+              내 프로필
+            </p>
+
+            <ProfileCard
+              name={name ?? ''}
+              phone={phone ?? ''}
+              address={address ?? ''}
+              bio={bio ?? ''}
+            />
+          </div>
+
+          <div className="bg-gray-5">
+            <RegisterCard
+              title="신청 내역"
+              description="아직 신청 내역이 없어요."
+              buttonText="공고 보러가기"
+            />
+          </div>
+        </>
+      ) : (
+        // 프로필 등록이 완료되지 않았을 때, 등록 컴포넌트 보이게
         <RegisterCard
-          title="신청 내역"
-          description="아직 신청 내역이 없어요."
-          buttonText="공고 보러가기"
+          title="내 프로필"
+          description="내 프로필을 등록하고 원하는 가게에 지원해 보세요."
+          buttonText="내 프로필 등록하기"
         />
-      </div>
+      )}
+      ;
     </>
   );
 }
