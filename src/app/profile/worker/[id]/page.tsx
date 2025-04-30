e8adb97a6b70218588c0f92a5b76153680699c62
이나경 <leena3823@gmail.com>
2025-04-30 21:32:04 +0900

    feat: data가 없을때 로직 처리

--- src/app/profile/worker/[id]/page.tsx
+++ src/app/profile/worker/[id]/page.tsx
@@ -10,15 +10,13 @@ export default function WorkerProfilePage() {
   const userId = params?.id as string;
   const { data } = useGetUser(userId);
 
-  if (!data) {
-    return <div>Loading...</div>;
-  }
+  if (!data) return null;
 
   const { name, phone, address, bio } = data.item;
 
   return (
     <>
-      {name ? ( // name이 있을 때, 즉 프로필 등록이 완료 되었을 때 보이는 UI
+      {name ? ( // name이 있을 때, 즉 프로필 등록이 완료 되었을 때 보이는 UI, 아래 RegisterCard는 테이블 완성되면 추후 분기 필요
         <>
           <div className="flex flex-col sm:flex-row w-full max-w-[964px] mx-auto px-6 py-[60px] gap-[24px] sm:gap-[180px]">
             <p className="sm:text-[28px] text-black font-spoqa text-[20px] font-bold leading-normal tracking-[0.56px] whitespace-nowrap">
@@ -42,7 +40,7 @@ export default function WorkerProfilePage() {
           </div>
         </>
       ) : (
-        // 프로필 등록이 완료되지 않았을 때, 등록 컴포넌트 보이게
+        // 프로필 등록이 완료되지 않았을 때, 등록 컴포넌트가 보이게 구현
         <RegisterCard
           title="내 프로필"
           description="내 프로필을 등록하고 원하는 가게에 지원해 보세요."
