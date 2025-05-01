ec7f5b0903d4b85f65f95225577630e632f88862
Na._.kyung <115490634+lee-nakyung@users.noreply.github.com>
2025-05-01 18:00:27 +0900

    ✨ [Feat] 내 프로필 카드 컴포넌트 api 연결 및 조건부 렌더링 (#59)
    * feat: 유저 정보 가져오기 react query 구현
    
    * feat: useParams로 userId 가져오기
    
    * feat: 프로필 등록 여부에 따라 조건부 렌더링 구현
    
    * feat: data가 없을때 로직 처리
    
    * ♻️ refactor: prettier 쌍따옴표로 설정 수정
    
    * feat: api 요청 한번만 실행
    
    * refactor: scaleTime 5분으로 변경
    
    * fix: address 타입 오류 수정

--- src/app/profile/worker/[id]/page.tsx
+++ src/app/profile/worker/[id]/page.tsx
@@ -1,27 +1,54 @@
-import ProfileCard from '@/app/profile/worker/[id]/components/ProfileCard';
-import RegisterCard from '@/components/Card/RegisterCard';
+"use client";
+
+import ProfileCard from "@/app/profile/worker/[id]/components/ProfileCard";
+import RegisterCard from "@/components/Card/RegisterCard";
+import { useParams } from "next/navigation";
+import { useGetUser } from "@/hooks/api/useUserService";
+import { SeoulDistrict } from "@/types/common";
 
 export default function WorkerProfilePage() {
+  const params = useParams();
+  const userId = params?.id as string;
+  const { data } = useGetUser(userId);
+
+  if (!data) return null;
+
+  const { name, phone, address, bio } = data.item;
+
   return (
     <>
-      <div className="flex flex-col sm:flex-row w-full max-w-[964px] mx-auto px-6 py-[60px] gap-[24px] sm:gap-[180px]">
-        <p className="sm:text-[28px] text-black font-spoqa text-[20px] font-bold leading-normal tracking-[0.56px] whitespace-nowrap">
-          내 프로필
-        </p>
-        <ProfileCard
-          name="김승우"
-          phone="010-1234-4321"
-          region="서울시 도봉구"
-          introduction="열심히 일 하겠습니다"
-        />
-      </div>
-      <div className="bg-gray-5">
+      {name ? ( // name이 있을 때, 즉 프로필 등록이 완료 되었을 때 보이는 UI, 아래 RegisterCard는 테이블 완성되면 추후 분기 필요
+        <>
+          <div className="flex flex-col sm:flex-row w-full max-w-[964px] mx-auto px-6 py-[60px] gap-[24px] sm:gap-[180px]">
+            <p className="sm:text-[28px] text-black font-spoqa text-[20px] font-bold leading-normal tracking-[0.56px] whitespace-nowrap">
+              내 프로필
+            </p>
+
+            <ProfileCard
+              name={name ?? ""}
+              phone={phone ?? ""}
+              address={address as SeoulDistrict}
+              bio={bio ?? ""}
+            />
+          </div>
+
+          <div className="bg-gray-5">
+            <RegisterCard
+              title="신청 내역"
+              description="아직 신청 내역이 없어요."
+              buttonText="공고 보러가기"
+            />
+          </div>
+        </>
+      ) : (
+        // 프로필 등록이 완료되지 않았을 때, 등록 컴포넌트가 보이게 구현
         <RegisterCard
-          title="신청 내역"
-          description="아직 신청 내역이 없어요."
-          buttonText="공고 보러가기"
+          title="내 프로필"
+          description="내 프로필을 등록하고 원하는 가게에 지원해 보세요."
+          buttonText="내 프로필 등록하기"
         />
-      </div>
+      )}
+      ;
     </>
   );
 }
