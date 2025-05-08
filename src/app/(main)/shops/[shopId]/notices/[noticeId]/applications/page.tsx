18f48afa760163717d8b179acd2657009c3edab3
K0seoyoung <134926158+K0seoyoung@users.noreply.github.com>
2025-05-08 17:45:50 +0900

    fix: shop-item 수정

--- src/app/(main)/shops/[shopId]/notices/[noticeId]/applications/page.tsx
+++ src/app/(main)/shops/[shopId]/notices/[noticeId]/applications/page.tsx
@@ -11,79 +11,79 @@ import { applicationColumns } from "@/components/Table/TableSchemas";
 import { Shop, ExtendedNotice } from "@/types/ShopTypes"; // Shop과 ExtendedNotice 타입 가져오기
 import { useParams } from "next/navigation";
 import RegisterCard from "@/components/Card/RegisterCard";
+import { useShopsNotice } from "@/hooks/api/useNoticeService";
 
 export default function NoticeListPage() {
-  const [offset, setOffset] = useState(0);
-  const limit = 5;
-  const params = useParams();
-  const shopId = params?.shopId as string;
-  const noticeId = params?.noticeId as string;
+    const [offset, setOffset] = useState(0);
+    const limit = 5;
+    const params = useParams();
+    const shopId = params?.shopId as string;
+    const noticeId = params?.noticeId as string;
 
-  // 지원자 목록 데이터 가져오기
-  const { data } = useApplicationList(shopId, noticeId, {
-    offset,
-    limit,
-  });
+    // 지원자 목록 데이터 가져오기
+    const { data: notice } = useShopsNotice(shopId, noticeId);
+    const { data } = useApplicationList(shopId, noticeId, {
+        offset,
+        limit,
+    });
+    if (!data) return null;
 
-  if (!data) return null;
+    // 지원자 목록 데이터 가공
+    const applications: Application[] =
+        data?.items.map((w) => ({
+            id: w.item.id,
+            status: w.item.status,
+            createdAt: w.item.createdAt,
+            user: w.item.user,
+            shop: w.item.shop,
+            notice: w.item.notice,
+        })) ?? [];
 
-  // 지원자 목록 데이터 가공
-  const applications: Application[] =
-    data?.items.map((w) => ({
-      id: w.item.id,
-      status: w.item.status,
-      createdAt: w.item.createdAt,
-      user: w.item.user,
-      shop: w.item.shop,
-      notice: w.item.notice,
-    })) ?? [];
+    // shopData와 noticeData 추출 및 타입 변환
+    const shopData = notice?.item.shop.item;
+    const noticeData = notice?.item;
 
-  // shopData와 noticeData 추출 및 타입 변환
-  const shopData: Shop | null = applications[0]?.shop?.item ?? null;
-  const noticeData: ExtendedNotice | null =
-    applications[0]?.notice?.item ?? null;
+    const handlePageChange = ({ offset }: { offset: number }) => {
+        setOffset(offset);
+    };
 
-  const handlePageChange = ({ offset }: { offset: number }) => {
-    setOffset(offset);
-  };
+    return (
+        <div>
+            {shopData && noticeData && (
+                <Section subname="식당" name={shopData.name}>
+                    <ShopItem
+                        shopId={shopId}
+                        noticeId={noticeId}
+                        shopData={shopData}
+                        noticeData={noticeData}
+                    />
+                </Section>
+            )}
 
-  return (
-    <div>
-      {shopData && noticeData && (
-        <Section subname="식당" name={shopData.name}>
-          <ShopItem
-            shopId={shopId}
-            noticeId={noticeId}
-            shopData={shopData}
-            noticeData={noticeData}
-          />
-        </Section>
-      )}
-
-      {applications.length === 0 ? (
-        <RegisterCard
-          title="신청 내역"
-          description="아직 신청자가 없어요."
-          buttonText="내 가게 보러가기"
-        />
-      ) : (
-        <Section name="신청자 목록">
-          <div className="space-y-6">
-            <Table<Application>
-              data={applications}
-              columns={applicationColumns}
-              pagination={
-                <Pagination
-                  totalItems={data.count}
-                  itemsPerPage={limit}
-                  currentOffset={offset}
-                  onPageChange={handlePageChange}
+            {applications.length === 0 ? (
+                <RegisterCard
+                    title="신청 내역"
+                    description="아직 신청자가 없어요."
+                    buttonText="내 가게 보러가기"
                 />
-              }
-            />
-          </div>
-        </Section>
-      )}
-    </div>
-  );
+            ) : (
+                <Section name="신청자 목록">
+                    <div className="space-y-6">
+                        <Table<Application>
+                            data={applications}
+                            columns={applicationColumns}
+                            pagination={
+                                <Pagination
+                                    totalItems={data.count}
+                                    itemsPerPage={limit}
+                                    currentOffset={offset}
+                                    onPageChange={handlePageChange}
+                                />
+                            }
+                        />
+                    </div>
+                </Section>
+            )}
+        </div>
+    );
 }
--- src/components/ShopItem/ShopItem.tsx
+++ src/components/ShopItem/ShopItem.tsx
@@ -1,93 +1,152 @@
-"use client"; // 클라이언트 컴포넌트로 선언
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import IconTextList from "../IconText/IconTextList";
 import clsx from "clsx";
 import {
-  ShopItemProps,
-  calculateEndDate,
-  formatDate,
-  formatTime,
-  calculateHourlyPayDiff,
+    ShopItemProps,
+    calculateEndDate,
+    formatDate,
+    formatTime,
+    calculateHourlyPayDiff,
 } from "./ShopItemSchemas";
 import { useRouter } from "next/navigation";
+import {
+    usePostApplication,
+    useUserApplicationList,
+} from "@/hooks/api/useApplications";
+import requestor from "@/lib/axios"; // 서버 요청을 위한 axios 인스턴스
 
 export default function ShopItem({
-  shopId,
-  noticeId,
-  shopData,
-  noticeData,
+    shopId,
+    noticeId,
+    shopData,
+    noticeData,
 }: ShopItemProps) {
-  const router = useRouter();
+    const router = useRouter();
+    const type = localStorage.getItem("type");
+    const apply = usePostApplication();
 
-  const startDate = new Date(noticeData.startsAt);
-  const endDate = calculateEndDate(noticeData.startsAt, noticeData.workhour);
+    const [isApply, setIsApply] = useState(false); // 지원 여부 상태
+    const userId = localStorage.getItem("id"); // 로컬 스토리지에서 사용자 ID 가져오기
+    const query = { offset: 0, limit: 10 }; // 필요한 쿼리 파라미터 설정
 
-  const startDateString = formatDate(startDate);
-  const endDateString = formatTime(endDate);
+    // 페이지 로드 시 지원 여부 확인
+    // const checkApplicationStatus = useUserApplicationList(userId, query);
+    // useEffect(() => {
+    //     if (checkApplicationStatus.data?.items) {
+    //         const appliedItem = checkApplicationStatus.data.items.find(
+    //             (item) => item.notice.item.id === noticeId
+    //         );
 
-  const iconTextItems = [
-    {
-      iconSrc: "/assets/images/clock.svg",
-      text: `${startDateString} ~ ${endDateString} (${noticeData.workhour}시간)`,
-    },
-    {
-      iconSrc: "/assets/images/map.svg",
-      text: `${shopData.address1}`,
-    },
-  ];
+    //         if (appliedItem) {
+    //             setIsApply(true);
+    //         }
+    //     }
+    // }, [checkApplicationStatus.data, noticeId]);
 
-  const { isPayDiff, isPayHigher, payDiffText } = calculateHourlyPayDiff(
-    noticeData.hourlyPay,
-    shopData.originalHourlyPay
-  );
-
-  return (
-    <div>
-      <div
-        className={clsx(
-          "p-4 border rounded-lg overflow-hidden",
-          "flex flex-col md:flex-row md:gap-x-6",
-          "md:flex-row p-[24px]"
-        )}
-      >
-        <img
-          src={shopData.imageUrl}
-          alt={shopData.name}
-          className={clsx(
-            "w-full max-w-[550px] h-[200px] object-cover rounded-lg mx-auto", // 모바일 기준
-            "sm:h-[300px]",
-            "md:h-[330px]"
-          )}
-        />
-        <div>
-          <p className="mt-4 text-sm font-bold text-red-50">시급</p>
-          <div className="mt-2 flex flex-row items-center space-x-2">
-            <p className="text-lg font-bold text-black text-[24px] sm:text-[28px]">
-              {noticeData.hourlyPay.toLocaleString()}원
-            </p>
-            {isPayDiff && (
-              <button className="rounded-full bg-red-50 text-white font-bold text-[12px] sm:text-[14px] py-2 px-4">
-                {payDiffText}
-              </button>
-            )}
-          </div>
-          <IconTextList items={iconTextItems} className="mt-4" />
-          <p className="mt-4 text-black">{shopData.description}</p>
-          <button
-            className="mt-6 w-full py-2 border border-red-50 text-red-50 font-bold rounded-md"
-            onClick={() =>
-              router.push(`shops/${shopId}/notices/${noticeId}/edit`)
+    const handleApply = () => {
+        apply.mutate(
+            { noticeId, shopId },
+            {
+                onSuccess: () => {
+                    setIsApply(true); // 지원 성공 시 상태 업데이트
+                    alert("공고 지원이 완료되었습니다!");
+                },
+                onError: (error) => {
+                    console.error("지원 실패:", error);
+                    alert("공고 지원에 실패했습니다. 다시 시도해주세요.");
+                },
             }
-          >
-            공고 편집하기
-          </button>
+        );
+    };
+
+    const startDate = new Date(noticeData.startsAt);
+    const endDate = calculateEndDate(noticeData.startsAt, noticeData.workhour);
+
+    const startDateString = formatDate(startDate);
+    const endDateString = formatTime(endDate);
+
+    const iconTextItems = [
+        {
+            iconSrc: "/assets/images/clock.svg",
+            text: `${startDateString} ~ ${endDateString} (${noticeData.workhour}시간)`,
+        },
+        {
+            iconSrc: "/assets/images/map.svg",
+            text: `${shopData.address1}`,
+        },
+    ];
+
+    const { isPayDiff, isPayHigher, payDiffText } = calculateHourlyPayDiff(
+        noticeData.hourlyPay,
+        shopData.originalHourlyPay
+    );
+
+    return (
+        <div>
+            <div
+                className={clsx(
+                    "p-4 border rounded-lg overflow-hidden",
+                    "flex flex-col md:flex-row md:gap-x-6",
+                    "md:flex-row p-[24px]"
+                )}
+            >
+                <img
+                    src={shopData.imageUrl}
+                    alt={shopData.name}
+                    className={clsx(
+                        "w-full max-w-[550px] h-[200px] object-cover rounded-lg mx-auto", // 모바일 기준
+                        "sm:h-[300px]",
+                        "md:h-[330px]"
+                    )}
+                />
+                <div>
+                    <p className="mt-4 text-sm font-bold text-red-50">시급</p>
+                    <div className="mt-2 flex flex-row items-center space-x-2">
+                        <p className="text-lg font-bold text-black text-[24px] sm:text-[28px]">
+                            {noticeData.hourlyPay.toLocaleString()}원
+                        </p>
+                        {isPayDiff && (
+                            <button className="rounded-full bg-red-50 text-white font-bold text-[12px] sm:text-[14px] py-2 px-4">
+                                {payDiffText}
+                            </button>
+                        )}
+                    </div>
+                    <IconTextList items={iconTextItems} className="mt-4" />
+                    <p className="mt-4 text-black">{shopData.description}</p>
+                    {type === "employer" ? (
+                        <button
+                            className="mt-6 w-full py-2 border border-red-50 text-red-50 font-bold rounded-md"
+                            onClick={() =>
+                                router.push(
+                                    `/shops/${shopId}/notices/${noticeId}/edit`
+                                )
+                            }
+                        >
+                            공고 편집하기
+                        </button>
+                    ) : type === "employee" ? (
+                        isApply ? (
+                            <button
+                                className="mt-6 w-full py-2 bg-gray-300 text-white font-bold rounded-md cursor-not-allowed"
+                                disabled
+                            >
+                                지원 완료
+                            </button>
+                        ) : (
+                            <button
+                                className="mt-6 w-full py-2 bg-50 text-white font-bold rounded-md"
+                                onClick={handleApply}
+                            >
+                                공고 지원하기
+                            </button>
+                        )
+                    ) : null}
+                </div>
+            </div>
+            <div className="flex flex-col mt-6 bg-gray-10 text-black rounded-lg p-[32px] gap-4">
+                <p className="font-bold text-black">공고 설명</p>
+                <p className="text-black">{noticeData.description}</p>
+            </div>
         </div>
-      </div>
-      <div className="flex flex-col mt-6 bg-gray-10 text-black rounded-lg p-[32px] gap-4">
-        <p className="font-bold text-black">공고 설명</p>
-        <p className="text-black">{noticeData.description}</p>
-      </div>
-    </div>
-  );
+    );
 }
